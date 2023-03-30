a511e68635afdb1ce33938a757656498690e109e
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-03-30 21:10:06 +0530

    Update Display Multiplication Table up to 10.cpp

--- Display Multiplication Table up to 10.cpp
+++ Display Multiplication Table up to 10.cpp
@@ -1,18 +1,14 @@
+//Display Multiplication Table up to 10
 #include <iostream>
 using namespace std;
-
 int main() {
-
     int n;
-
     cout << "Enter a positive integer: ";
     cin >> n;
-
     // run a loop from 1 to 10
     // print the multiplication table
     for (int i = 1; i <= 10; ++i) {
         cout << n << " * " << i << " = " << n * i << endl;
     }
-    
     return 0;
 }
